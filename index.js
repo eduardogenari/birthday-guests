09917dff5744d9a8130850a5dc55b873781cac40
Eduardo Genari <67627261+eduardogenari@users.noreply.github.com>
2022-08-29 16:30:42 +0200

    add files

---
+++ index.js
@@ -0,0 +1,7 @@
+const guestList = ['Tom', 'Mary', 'Clare', 'John', 'Liz']
+
+const guestsHtml = guestList.map(function(guest){
+    return `<div class="box">${guest}</div>`
+})
+
+document.getElementById('list').innerHTML = guestsHtml.join('')
